91c29229680c4e3641bbe794f014f6c644b4025c
dalhun namgung <bnamgung@gmail.com>
2017-12-04 19:17:22 -0800

    dependecies installed

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,22 @@
 'use strict';
 
 
-
+//script & style tags
 const HTMLPlugin = require('html-webpack-plugin');
 const ExtractPlugin = require('extract-text-weback-plugin');
+//create css bundle
 
 
 module.exports = {
   entry: `$__dirname}/src/main.js`,
-
+//load this
   devtool: 'source-map',
 
   output: {
     filename: 'bundle.[hash].js',
     path: `$__dirname}/build`
   },
-
+//put into path folder
 
   plugins: [
     new HTMLPlugin({
@@ -25,14 +26,14 @@ module.exports = {
     new ExtractPlugin('bundle.[hash].css');
   ],
 
-
+//if js file not in node_modules, use babel load
   module: {
     rules: [{
       test: /\.js$/,
       exclude: /node_modules/,
       loader: 'babel-loader'
     },
-
+//if .scss file, these get loaded in reverse order and the output of one pipe
     {
       test: /\.scss$/,
       loader: ExtractPlugin.extract({
@@ -55,6 +56,7 @@ module.exports = {
     },
     }]
   };
+
 
 
 
